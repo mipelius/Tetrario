0c28eda29caccdac7d1894ed6c64ae172d11d589
Miika Pelkonen <pelkonen.miika@gmail.com>
2018-04-22 09:06:52 +0300

    added multiple levels

---
+++ LevelExit.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+export(String, FILE, "*.tscn") var scene_to_load
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "Player":
+			get_tree().change_scene(scene_to_load)
